3e4d18d83ef86d692aa4773c589d751b574eb98f
hoangtuan2000 <duonghoangtuan.td@gmail.com>
2022-09-20 14:47:02 +0700

    update API filter schedule of user

--- controllers/controllersUser/scheduleUserControllers.js
+++ controllers/controllersUser/scheduleUserControllers.js
@@ -8,24 +8,101 @@ const {
 } = require("../controllersGlobal/scheduleControllers");
 
 const getUserRegisteredScheduleList = async (req, res) => {
-    let { page, limitEntry } = req.body;
+    let {
+        page,
+        limitEntry,
+        status,
+        carType,
+        scheduleCode,
+        address,
+        idWard,
+        startDate,
+        endDate,
+    } = req.body;
     page = parseInt(page) || Constants.Common.PAGE;
     limitEntry = parseInt(limitEntry) || Constants.Common.LIMIT_ENTRY;
     let data = { ...Constants.ResultData };
     let dataList = { ...Constants.ResultDataList };
 
     if (req.userToken) {
-        const idUser = req.userToken.idUser;
-        const resultExecuteQuery = await executeQuery(
-            db,
-            `SELECT COUNT(idSchedule) as sizeQuerySnapshot FROM schedule WHERE idUser = ${idUser}`
-        );
-        if (!resultExecuteQuery) {
-            data.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
-            data.message = Strings.Common.ERROR_GET_DATA;
+        if (
+            (status && !helper.isArray(status)) ||
+            (carType && !helper.isArray(carType))
+        ) {
+            data.status = Constants.ApiCode.BAD_REQUEST;
+            data.message = Strings.Common.INVALID_DATA;
             res.status(200).send(data);
         } else {
-            if (resultExecuteQuery.length > 0) {
+            const idUser = req.userToken.idUser;
+            let sqlExecuteQuery = `SELECT COUNT(idSchedule) as sizeQuerySnapshot FROM schedule as sc, car as ca, car_type as ct
+            WHERE sc.idUser = ${idUser} AND sc.idCar = ca.idCar AND ca.idCarType = ct.idCarType `;
+
+            let conditionSql = "";
+            if (status && status.length > 0) {
+                let sqlTemp = "";
+                for (let i = 0; i < status.length; i++) {
+                    if (i == 0) {
+                        if (status.length > 1) {
+                            sqlTemp += ` AND ( sc.idScheduleStatus = '${status[i]}' `;
+                        } else {
+                            sqlTemp += ` AND ( sc.idScheduleStatus = '${status[i]}' ) `;
+                        }
+                    } else if (i == status.length - 1) {
+                        sqlTemp += ` OR sc.idScheduleStatus = '${status[i]}' ) `;
+                    } else {
+                        sqlTemp += ` OR sc.idScheduleStatus = '${status[i]}' `;
+                    }
+                }
+                conditionSql += sqlTemp;
+            }
+            if (carType && carType.length > 0) {
+                let sqlTemp = "";
+                for (let i = 0; i < carType.length; i++) {
+                    if (i == 0) {
+                        if (carType.length > 1) {
+                            sqlTemp += ` AND ( ct.idCarType = '${carType[i]}' `;
+                        } else {
+                            sqlTemp += ` AND ( ct.idCarType = '${carType[i]}' ) `;
+                        }
+                    } else if (i == carType.length - 1) {
+                        sqlTemp += ` OR ct.idCarType = '${carType[i]}' ) `;
+                    } else {
+                        sqlTemp += ` OR ct.idCarType = '${carType[i]}' `;
+                    }
+                }
+                conditionSql += sqlTemp;
+            }
+            if (!helper.isNullOrEmpty(scheduleCode)) {
+                conditionSql += ` AND (sc.idSchedule = '${scheduleCode}') `;
+            }
+            if (!helper.isNullOrEmpty(address)) {
+                conditionSql += ` AND (sc.startLocation LIKE '%${address}%') `;
+            }
+            if (!helper.isNullOrEmpty(idWard)) {
+                conditionSql += ` AND (sc.idWardStartLocation = '${idWard}' OR sc.idWardEndLocation = '${idWard}') `;
+            }
+            if (
+                helper.formatTimeStamp(startDate) &&
+                helper.formatTimeStamp(endDate)
+            ) {
+                const startTimeStamp = helper.formatTimeStamp(startDate);
+                const endTimeStamp = helper.formatTimeStamp(endDate);
+                conditionSql += ` AND (( DATE(FROM_UNIXTIME(${startTimeStamp})) BETWEEN DATE(FROM_UNIXTIME(sc.startDate)) AND DATE(FROM_UNIXTIME(sc.endDate))) 
+                OR ( DATE(FROM_UNIXTIME(${endTimeStamp})) BETWEEN DATE(FROM_UNIXTIME(sc.startDate)) AND DATE(FROM_UNIXTIME(sc.endDate))) 
+                OR (DATE(FROM_UNIXTIME(sc.startDate)) BETWEEN DATE(FROM_UNIXTIME(${startTimeStamp})) AND DATE(FROM_UNIXTIME(${endTimeStamp}))) 
+                OR (DATE(FROM_UNIXTIME(sc.endDate)) BETWEEN DATE(FROM_UNIXTIME(${startTimeStamp})) AND DATE(FROM_UNIXTIME(${endTimeStamp})))) `;
+            }
+
+            const resultExecuteQuery = await executeQuery(
+                db,
+                `${sqlExecuteQuery} ${conditionSql}`
+            );
+
+            if (!resultExecuteQuery) {
+                data.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
+                data.message = Strings.Common.ERROR_GET_DATA;
+                res.status(200).send(data);
+            } else {
                 let sql = `SELECT
                                 sc.idSchedule, sc.reason, sc.startDate, sc.endDate, sc.endLocation,
                                 ca.idCar, ca.image, ca.licensePlates, ca.idCarType,
@@ -41,7 +118,7 @@ const getUserRegisteredScheduleList = async (req, res) => {
                             LEFT JOIN district as de ON de.idDistrict = we.idDistrict
                             LEFT JOIN province as pe ON pe.idProvince = de.idProvince
                             LEFT JOIN review as re ON re.idSchedule = sc.idSchedule
-                            WHERE sc.idUser = ?
+                            WHERE sc.idUser = ? ${conditionSql}
                             ORDER BY FROM_UNIXTIME(sc.startDate) DESC
                             LIMIT ${
                                 limitEntry * page - limitEntry
@@ -62,14 +139,6 @@ const getUserRegisteredScheduleList = async (req, res) => {
                         res.status(200).send(dataList);
                     }
                 });
-            } else {
-                dataList.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
-                dataList.message = Strings.Common.ERROR_GET_DATA;
-                dataList.limitEntry = limitEntry;
-                dataList.page = page;
-                dataList.sizeQuerySnapshot =
-                    resultExecuteQuery[0].sizeQuerySnapshot;
-                res.status(200).send(dataList);
             }
         }
     } else {
