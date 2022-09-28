a06e92c3c05b90f43b8bacbb6e6885c9dc069792
hoangtuan2000 <duonghoangtuan.td@gmail.com>
2022-09-28 19:42:49 +0700

    update API filter car list manager of admin

--- controllers/controllersAdmin/carAdminControllers.js
+++ controllers/controllersAdmin/carAdminControllers.js
@@ -5,25 +5,105 @@ const { Strings } = require("../../constants/Strings");
 const db = require("../../models/index");
 
 const getCarListForAdmin = async (req, res) => {
-    let { page, limitEntry } = req.body;
+    let {
+        page,
+        limitEntry,
+        carStatus,
+        carType,
+        carBrand,
+        licensePlates,
+        carCode,
+    } = req.body;
     page = parseInt(page) || Constants.Common.PAGE;
     limitEntry = parseInt(limitEntry) || Constants.Common.LIMIT_ENTRY;
     let data = { ...Constants.ResultData };
     let dataList = { ...Constants.ResultDataList };
 
     if (req.userToken) {
-        const idUser = req.userToken.idUser;
-        let sqlExecuteQuery = `SELECT COUNT(idCar) as sizeQuerySnapshot FROM car `;
-
-        // EXECUTE SQL
-        const resultExecuteQuery = await executeQuery(db, sqlExecuteQuery);
-
-        if (!resultExecuteQuery) {
-            data.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
-            data.message = Strings.Common.ERROR_GET_DATA;
+        if (
+            (carStatus && !helper.isArray(carStatus)) ||
+            (carType && !helper.isArray(carType)) ||
+            (carBrand && !helper.isArray(carBrand))
+        ) {
+            data.status = Constants.ApiCode.BAD_REQUEST;
+            data.message = Strings.Common.INVALID_DATA;
             res.status(200).send(data);
         } else {
-            const sql = `SELECT 
+            let sqlExecuteQuery = `SELECT COUNT(ca.idCar) as sizeQuerySnapshot 
+                                    FROM car as ca
+                                    LEFT JOIN car_type as ct ON ct.idCarType = ca.idCarType
+                                    LEFT JOIN car_status as cs ON cs.idCarStatus = ca.idCarStatus
+                                    LEFT JOIN car_brand as cb ON cb.idCarBrand = ca.idCarBrand
+                                    WHERE 1 = 1 `;
+            // CHECK CONDITION SQL
+            let conditionSql = "";
+            if (carType && carType.length > 0) {
+                let sqlTemp = "";
+                for (let i = 0; i < carType.length; i++) {
+                    if (i == 0) {
+                        if (carType.length > 1) {
+                            sqlTemp += ` AND ( ct.idCarType = '${carType[i]}' `;
+                        } else {
+                            sqlTemp += ` AND ( ct.idCarType = '${carType[i]}' ) `;
+                        }
+                    } else if (i == carType.length - 1) {
+                        sqlTemp += ` OR ct.idCarType = '${carType[i]}' ) `;
+                    } else {
+                        sqlTemp += ` OR ct.idCarType = '${carType[i]}' `;
+                    }
+                }
+                conditionSql += sqlTemp;
+            }
+            if (carStatus && carStatus.length > 0) {
+                let sqlTemp = "";
+                for (let i = 0; i < carStatus.length; i++) {
+                    if (i == 0) {
+                        if (carStatus.length > 1) {
+                            sqlTemp += ` AND ( cs.idCarStatus = '${carStatus[i]}' `;
+                        } else {
+                            sqlTemp += ` AND ( cs.idCarStatus = '${carStatus[i]}' ) `;
+                        }
+                    } else if (i == carStatus.length - 1) {
+                        sqlTemp += ` OR cs.idCarStatus = '${carStatus[i]}' ) `;
+                    } else {
+                        sqlTemp += ` OR cs.idCarStatus = '${carStatus[i]}' `;
+                    }
+                }
+                conditionSql += sqlTemp;
+            }
+            if (carBrand && carBrand.length > 0) {
+                let sqlTemp = "";
+                for (let i = 0; i < carBrand.length; i++) {
+                    if (i == 0) {
+                        if (carBrand.length > 1) {
+                            sqlTemp += ` AND ( cb.idCarBrand = '${carBrand[i]}' `;
+                        } else {
+                            sqlTemp += ` AND ( cb.idCarBrand = '${carBrand[i]}' ) `;
+                        }
+                    } else if (i == carBrand.length - 1) {
+                        sqlTemp += ` OR cb.idCarBrand = '${carBrand[i]}' ) `;
+                    } else {
+                        sqlTemp += ` OR cb.idCarBrand = '${carBrand[i]}' `;
+                    }
+                }
+                conditionSql += sqlTemp;
+            }
+            if (!helper.isNullOrEmpty(carCode)) {
+                conditionSql += ` AND (ca.idCar = '${carCode}') `;
+            }
+            if (!helper.isNullOrEmpty(licensePlates)) {
+                conditionSql += ` AND (ca.licensePlates LIKE '%${licensePlates}%') `;
+            }
+
+            // EXECUTE SQL
+            const resultExecuteQuery = await executeQuery(db, `${sqlExecuteQuery} ${conditionSql}`);
+
+            if (!resultExecuteQuery) {
+                data.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
+                data.message = Strings.Common.ERROR_GET_DATA;
+                res.status(200).send(data);
+            } else {
+                const sql = `SELECT 
                             ca.idCar, ca.licensePlates, ca.image,
                             IFNULL(carSchedule.numberOfTrips, 0) as numberOfTrips,
                             IFNULL(licenseExpired.licenseNumberExpired , 0) as licenseNumberExpired,
@@ -37,39 +117,39 @@ const getCarListForAdmin = async (req, res) => {
                         LEFT JOIN car_status as cs ON cs.idCarStatus = ca.idCarStatus
                         LEFT JOIN 
                             (SELECT COUNT(schedule.idCar) as numberOfTrips, car.idCar
-                            FROM car 
-                            RIGHT JOIN schedule ON car.idCar = schedule.idCar
-                            GROUP BY schedule.idCar) as carSchedule ON carSchedule.idCar = ca.idCar
+                                FROM car 
+                                RIGHT JOIN schedule ON car.idCar = schedule.idCar
+                                GROUP BY schedule.idCar) as carSchedule ON carSchedule.idCar = ca.idCar
                         LEFT JOIN 
                             (SELECT COUNT(idCar) as numberOfFailures, idCar
-                            FROM car_breakdown
-                            GROUP BY idCar) as cbd ON cbd.idCar = ca.idCar
+                                FROM car_breakdown
+                                GROUP BY idCar) as cbd ON cbd.idCar = ca.idCar
                         LEFT JOIN
                             (SELECT COUNT(cld.idCar) as licenseNumberExpired, cld.idCar
-                            FROM car as ca
-                            RIGHT JOIN car_license_detail as cld 
-                            ON ca.idCar = cld.idCar 
-                            AND DATE(FROM_UNIXTIME(cld.carLicenseExpirationDate)) <= CURRENT_DATE()
-                            GROUP BY cld.idCar) as licenseExpired ON licenseExpired.idCar = ca.idCar        
-                        LIMIT ${
-                            limitEntry * page - limitEntry
-                        }, ${limitEntry}`;
-            db.query(sql, (err, result) => {
-                if (err) {
-                    data.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
-                    data.message = Strings.Common.ERROR;
-                    res.status(200).send(data);
-                } else {
-                    dataList.status = Constants.ApiCode.OK;
-                    dataList.message = Strings.Common.SUCCESS;
-                    dataList.limitEntry = limitEntry;
-                    dataList.page = page;
-                    dataList.sizeQuerySnapshot =
-                        resultExecuteQuery[0].sizeQuerySnapshot;
-                    dataList.data = [...result];
-                    res.status(200).send(dataList);
-                }
-            });
+                                FROM car as ca
+                                RIGHT JOIN car_license_detail as cld 
+                                ON ca.idCar = cld.idCar 
+                                AND DATE(FROM_UNIXTIME(cld.carLicenseExpirationDate)) <= CURRENT_DATE()
+                                GROUP BY cld.idCar) as licenseExpired ON licenseExpired.idCar = ca.idCar  
+                        WHERE 1 = 1 ${conditionSql}   
+                        LIMIT ${limitEntry * page - limitEntry}, ${limitEntry}`;
+                db.query(sql, (err, result) => {
+                    if (err) {
+                        data.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
+                        data.message = Strings.Common.ERROR;
+                        res.status(200).send(data);
+                    } else {
+                        dataList.status = Constants.ApiCode.OK;
+                        dataList.message = Strings.Common.SUCCESS;
+                        dataList.limitEntry = limitEntry;
+                        dataList.page = page;
+                        dataList.sizeQuerySnapshot =
+                            resultExecuteQuery[0].sizeQuerySnapshot;
+                        dataList.data = [...result];
+                        res.status(200).send(dataList);
+                    }
+                });
+            }
         }
     } else {
         data.status = Constants.ApiCode.INTERNAL_SERVER_ERROR;
